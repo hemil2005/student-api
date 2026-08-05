62ce78b5ba7d39a13844fbdc060aa564be25d3ca
hemil2005 <hemil.shah2020@gmail.com>
2026-08-05 16:23:20 +0530

    Add PostgreSQL support

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import app from './app.js';
+import pool from './src/config/database.js'
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+const result = await pool.query("SELECT NOW();")
