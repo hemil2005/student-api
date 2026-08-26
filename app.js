e9216451b12c29e463dfbabf250aa9a5bf703850
hemil2005 <hemil.shah2020@gmail.com>
2026-08-26 14:10:34 +0530

    mount swagger in app.js

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ import studentRoutes from './src/routes/student.route.js'
 import userRoutes from './src/routes/user.route.js'
 import courseRoutes from './src/routes/course.route.js'
 import { errorHandler } from './src/middleware/error.middleware.js'
+import swaggerUi from "swagger-ui-express";
+import openapiSpecification from "./src/docs/openapi.js";
 const app = express();
 app.use(express.json());
-
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(openapiSpecification)
+);
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to the Student API Home Directory!" });
 });
---
+++ src/docs/openapi.js
@@ -0,0 +1,19 @@
+const openapiSpecification = {
+    openapi: "3.0.0",
+
+    info: {
+        title: "Student API",
+        version: "1.0.0",
+        description: "API documentation for the Student Management API"
+    },
+
+    servers: [
+        {
+            url: "http://localhost:3000"
+        }
+    ],
+
+    paths: {}
+};
+
+export default openapiSpecification;
